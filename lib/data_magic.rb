ed5c119ceb87b4543ea3295a456739b4d322ae55
Sarah Allen <sarah@ultrasaurus.com>
2015-05-28 19:18:16 -0700

    wip;

--- lib/data_magic.rb
+++ lib/data_magic.rb
@@ -1,17 +1,34 @@
 class DataMagic
   class << self
     require 'csv'
+    require 'elasticsearch'
+    @@eclient = Elasticsearch::Client.new log: true
+
+    def eclient
+      puts "--------- eclient: #{@@eclient}"
+      @@eclient
+    end
 
     def import_csv(datafile)
+
       data = datafile.read
       puts data
       fields = nil
       num_rows = 0
-      CSV.parse(data, headers:true, :header_converters=> lambda {|f| f.strip}) do |row|
+      CSV.parse(data, headers:true, :header_converters=> lambda {|f| f.strip.to_sym }) do |row|
         fields ||= row.headers
+        params = row
+        params[:index] = 'myindex'
+        params[:type] = 'mydata'
+
+        puts params.inspect
+        eclient.index params
+
         num_rows += 1
       end
 
+      puts "indexing complete"
+      puts client.search index: 'myindex',  query: { match: { content: 'love' } }
       return [num_rows, fields ]
     end
 
--- spec/lib/data_magic_spec.rb
+++ spec/lib/data_magic_spec.rb
@@ -4,10 +4,10 @@ require 'data_magic'
 describe DataMagic do
   scenario "#import_csv" do
     data_str = <<-eos
-      a,b
-      1,2
-      3,4
-    eos
+a,b
+1,2
+3,4
+eos
     data = StringIO.new(data_str)
     num_rows, fields = DataMagic.import_csv(data)
     expect(num_rows).to be(2)
