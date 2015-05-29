8ac06b2f93053f1c24a4b169d98c13d2e707c0a3
Sarah Allen <sarah@ultrasaurus.com>
2015-05-28 20:38:03 -0700

    term_vector mapping - experimental

--- lib/data_magic.rb
+++ lib/data_magic.rb
@@ -10,7 +10,29 @@ class DataMagic
     end
 
     def import_csv(datafile)
+      index_name = 'myindex'
+      type_name = 'mydata'
 
+      eclient.indices.delete index: '_all'
+      eclient.indices.create index: index_name,
+        body: {
+          mappings: {
+            mydata: {
+              properties: {
+                content: {
+                  type: 'string',
+                  term_vector: 'with_positions_offsets_payloads',
+                  store: "yes",
+                  term_statistics: true,
+                  analyzer: "whitespace"
+                }
+              }
+            }
+          }
+        }
+      puts "-"*80
+      puts eclient.indices.get_mapping
+      puts "-"*80
       data = datafile.read
       puts data
       fields = nil
@@ -20,13 +42,24 @@ class DataMagic
         row = row.to_hash
         puts row.class
         puts row.inspect
-        eclient.index index:'myindex', type:'mydata', body: row
+        eclient.index index: index_name, type:type_name, body: row, refresh: true
 
         num_rows += 1
       end
 
       puts "indexing complete"
-      puts eclient.search index: 'myindex', body: { query: { match: { content: 'love' } } }
+      result = eclient.search index: index_name, body: { query: { match: { content: 'love' } } }
+      puts result
+      puts "-"*80
+      puts result["hits"]
+      puts "-hits-"
+      puts result["hits"].class
+      result["hits"]["hits"].each do |hit|
+        puts id = hit["_id"]
+        eclient.termvector index: index_name, type: type_name, id: id
+
+      end
+
       return [num_rows, fields ]
     end
 
--- spec/lib/data_magic_spec.rb
+++ spec/lib/data_magic_spec.rb
@@ -4,13 +4,14 @@ require 'data_magic'
 describe DataMagic do
   scenario "#import_csv" do
     data_str = <<-eos
-a,b
-1,2
-3,4
+num,content
+1,this is something awesome
+2,You love this thing
+2,I love you love me
 eos
     data = StringIO.new(data_str)
     num_rows, fields = DataMagic.import_csv(data)
-    expect(num_rows).to be(2)
-    expect(fields).to eq( [:a,:b] )
+    expect(num_rows).to be(3)
+    expect(fields).to eq( [:num,:content] )
   end
 end
